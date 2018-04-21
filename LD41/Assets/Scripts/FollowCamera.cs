ef218e90cc3988e55d545ab07b22abc9fda770d5
ajweeks <ajweeks@shaw.ca>
2018-04-21 13:17:44 +0100

    Initial commit

---
+++ LD41/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public PlayerController pc;
+
+    // How quickly to rotate the camera towards the player
+    public float FollowSpeed = 1.1f;
+
+    // How much above the player to look at
+    public float ViewVerticalOffset = 5.0f;
+    public float VelocityScale = 1.0f;
+
+    private Vector3 pcOffset;
+
+    private Vector3 vel;
+
+	void Start () {
+        pcOffset = transform.position - pc.transform.position;
+    }
+	
+	void FixedUpdate () {
+
+        vel = pc.rb.velocity;
+
+        transform.position = pc.transform.position + pcOffset + -vel * VelocityScale * Time.fixedDeltaTime;
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation, 
+            Quaternion.LookRotation((pc.transform.position + new Vector3(0, ViewVerticalOffset, 0)) - transform.position, Vector3.up),
+            Time.fixedDeltaTime * FollowSpeed);
+	}
+}
---
+++ LD41/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public FollowCamera followCamera;
+
+    public float SidewaysMoveForce = 10000.0f;
+    public float ForwardMoveForce = 100.0f;
+
+    [HideInInspector]
+    public Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate () {
+        Vector3 force = new Vector3();
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = followCamera.transform.forward;
+        forward.y = 0.0f;
+        forward.Normalize();
+        Vector3 right = followCamera.transform.right;
+
+        force += right * horizontal * SidewaysMoveForce * Time.fixedDeltaTime;
+        force += forward * vertical * ForwardMoveForce * Time.fixedDeltaTime;
+
+        rb.AddForce(force);
+
+        Debug.DrawLine(transform.position, transform.position + forward * 10, Color.blue);
+        Debug.DrawLine(transform.position, transform.position + right * 10, Color.red);
+    }
+}
